085b09425ea486c718d6b0735d8ce4300368051b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2023-12-08 11:11:55 +0530

    updated

--- Servlet_Anotation/src/main/java/lk/ijse/gdse/hello/api/Hello_Servlet.java
+++ Servlet_Anotation/src/main/java/lk/ijse/gdse/hello/api/Hello_Servlet.java
@@ -1,11 +1,13 @@
 package lk.ijse.gdse.hello.api;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "Hello_Servlet",value = "/text",loadOnStartup = 1)
 public class Hello_Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
