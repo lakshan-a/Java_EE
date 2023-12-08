f6459cbb9c02ffb7efbfaf99a6a3a664c8f29db4
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2023-12-08 11:30:43 +0530

    updated

--- Servlet_Anotation/src/main/java/lk/ijse/gdse/hello/api/Hello_Servlet.java
+++ Servlet_Anotation/src/main/java/lk/ijse/gdse/hello/api/Hello_Servlet.java
@@ -1,13 +1,18 @@
 package lk.ijse.gdse.hello.api;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Hello_Servlet",value = "/text",loadOnStartup = 1)
+@WebServlet(name = "Hello_Servlet",value = "/text",loadOnStartup = 1,initParams = {
+        @WebInitParam(name = "username" , value = "root"),
+        @WebInitParam(name = "password" , value = "12345"),
+        @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/gdse66_hello")
+})
 public class Hello_Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
