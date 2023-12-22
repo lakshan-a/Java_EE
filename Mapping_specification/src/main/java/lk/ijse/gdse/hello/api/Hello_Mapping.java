aaf11d4d818e13890cb137ea6f14758f6a8f9cc4
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2023-12-22 13:39:01 +0530

    updated

---
+++ Mapping_specification/src/main/java/lk/ijse/gdse/hello/api/Hello_Mapping.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.hello.api;
+
+import com.sun.xml.internal.ws.transport.http.HttpAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Hello_Mapping" , value = "/hello")
+
+public class Hello_Mapping extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1> Hello, I am Lakshan </h1>");
+    }
+}
