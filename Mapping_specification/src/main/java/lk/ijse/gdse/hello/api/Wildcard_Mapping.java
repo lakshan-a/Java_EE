57c6b95c0c613b15e0a7479decbc9d8010b5e7ae
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2023-12-22 14:07:46 +0530

    updated

---
+++ Mapping_specification/src/main/java/lk/ijse/gdse/hello/api/Wildcard_Mapping.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse.hello.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Wildcard_Mapping" , value = "/*")
+
+
+public class Wildcard_Mapping extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>" +
+                "<body>" +
+                "<h1>Hello I am Lakshan </h1>" +
+                "</body>" +
+                "</html>");
+    }
+}
