5d58d68f3130112321729ec21bbe7e01ec08e55b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 16:42:31 +0530

    updated

--- Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
+++ Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
@@ -1,6 +1,10 @@
 package lk.ijse.gdse.hello.api;
 
 import com.sun.xml.internal.ws.transport.http.HttpAdapter;
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -72,14 +76,14 @@ public class Form_Action extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String id =req.getParameter("id");
-        String name =req.getParameter("name");
-        String address =req.getParameter("address");
         Connection connection = null;
 
-        System.out.printf("id=%s, name=%s, address=%s\n ", id, name, address);
+//        String id =req.getParameter("id");
+//        String name =req.getParameter("name");
+//        String address =req.getParameter("address");
+//        System.out.printf("id=%s, name=%s, address=%s\n ", id, name, address);
 
-     /*   Connection connection = null;
+
         PrintWriter writer =resp.getWriter();
 
         try {
@@ -97,6 +101,9 @@ public class Form_Action extends HttpServlet {
                 System.out.printf("id=%s, name=%s, address=%s\n ", id, name, address);
                 String jsonObject = "{\" id \": \""+ "\" name \":\""+ name + "\","+"\" address \":\""+ address + "\"}";
                 jsonArray += jsonObject + ",";
+
+
+
             }
 
             jsonArray = "["+ jsonArray.substring(0,jsonArray.length()-1) + "]";
@@ -115,7 +122,7 @@ public class Form_Action extends HttpServlet {
                     throwables.printStackTrace();
                 }
             }
-        }*/
+        }
 
 
     }
@@ -129,5 +136,60 @@ public class Form_Action extends HttpServlet {
         Connection connection = null;
 
         System.out.printf("id=%s, name=%s, address=%s\n ", id, name, address);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        JsonArrayBuilder list = Json.createArrayBuilder();
+
+
+        Connection connection = null;
+
+
+        PrintWriter writer =resp.getWriter();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection(url,username,password);
+            PreparedStatement stm =connection.prepareStatement("SELECT * FROM customer");
+            ResultSet res = stm.executeQuery();
+
+            String jsonArray = "";
+
+            while (res.next()){
+
+                JsonObjectBuilder builder = Json.createObjectBuilder();
+                builder.add("id",res.getString(1));
+                builder.add("name",res.getString(2));
+                builder.add("address",res.getString(3));
+
+                list.add(builder.build());
+
+            }
+
+
+            resp.setContentType("application/json");
+            resp.getWriter().println(list.build());
+
+        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+            throw new RuntimeException(e);
+
+        }finally {
+            if (connection !=null){
+                try{
+                    connection.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }
+
+
+
+
+
+
     }
 }
