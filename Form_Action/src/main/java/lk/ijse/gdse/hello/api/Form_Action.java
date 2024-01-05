9d6e53bcfd92c789a77531e2265b55033a8ff1b7
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 11:30:00 +0530

    updated

--- Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
+++ Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(name = "Customer" , urlPatterns = "/Customer", loadOnStartup = 1, initParams = {
         @WebInitParam(name = "username" , value = "root"),
@@ -23,11 +27,52 @@ public class Form_Action extends HttpServlet {
     private String url;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id =req.getParameter("id");
         String name =req.getParameter("name");
         String address =req.getParameter("address");
         String salary =req.getParameter("salary");
+        Connection connection = null;
+
+        System.out.printf("id=%s, name=%s, address=%s, salary=%s\n ", id, name, address,salary);
+
+
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            connection= DriverManager.getConnection(url,username,password);
+//            PreparedStatement stm =connection.prepareStatement("INSERT INTO customer(id,name,address,salary) VALUE (?,?,?,?)");
+//
+//            stm.setString(1,id);
+//            stm.setString(2,name);
+//            stm.setString(3,address);
+//            stm.setString(4,salary);
+//            stm.executeUpdate();
+//
+//
+//        } catch (ClassNotFoundException | SQLException e) {
+////            e.printStackTrace();
+//            throw new RuntimeException(e);
+//
+//        }finally {
+//            if (connection !=null){
+//                try{
+//                    connection.close();
+//                } catch (SQLException throwables) {
+//                    throwables.printStackTrace();
+//                }
+//            }
+//        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println();
 
 
     }
