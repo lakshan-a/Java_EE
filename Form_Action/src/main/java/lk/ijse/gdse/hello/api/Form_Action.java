448fb0904d9bd26af2f37c342a684dde0ab69c4c
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 11:08:12 +0530

    updated

--- Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
+++ Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
@@ -3,15 +3,24 @@ package lk.ijse.gdse.hello.api;
 import com.sun.xml.internal.ws.transport.http.HttpAdapter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Exact_Mapping" , value = "/hello")
+@WebServlet(name = "Customer" , urlPatterns = "/Customer", loadOnStartup = 1, initParams = {
+        @WebInitParam(name = "username" , value = "root"),
+        @WebInitParam(name = "password" , value = "12345"),
+        @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/gdse66_hello")
+})
 
 public class Form_Action extends HttpServlet {
+
+    private String username;
+    private String password;
+    private String url;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -19,5 +28,7 @@ public class Form_Action extends HttpServlet {
         String name =req.getParameter("name");
         String address =req.getParameter("address");
         String salary =req.getParameter("salary");
+
+
     }
 }
