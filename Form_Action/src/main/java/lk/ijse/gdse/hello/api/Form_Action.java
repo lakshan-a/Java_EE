ac81ab52991a2d768e1b72656c4aab823e0cfd73
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 10:36:43 +0530

    updated

---
+++ Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
@@ -0,0 +1,17 @@
+package lk.ijse.gdse.hello.api;
+
+import com.sun.xml.internal.ws.transport.http.HttpAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Form_Action extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
