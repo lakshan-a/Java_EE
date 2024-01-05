8443abb3f01bf52c1ffaa78c44d219de3ad4c7e0
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 09:34:02 +0530

    updated

--- Mapping_specification/src/main/java/lk/ijse/gdse/hello/api/Exact_Mapping.java
+++ Mapping_specification/src/main/java/lk/ijse/gdse/hello/api/Exact_Mapping.java
@@ -17,20 +17,20 @@ public class Exact_Mapping extends HttpServlet {
         PrintWriter writer = resp.getWriter();
         writer.println("<h1>Hello I am Exact Mapping (/**) </h1>");
 
-        System.out.println(req.getContextPath());
-        System.out.println(req.getPathInfo());
-        System.out.println(req.getServletPath());
-        System.out.println(req.getPathTranslated());
-        System.out.println(req.getQueryString());
-        System.out.println(req.getRequestURI());
-        System.out.println(req.getRequestURL());
-        System.out.println(req.getProtocol());
-        System.out.println(req.getScheme());
-        System.out.println(req.getRemoteAddr());
-        System.out.println(req.getRemotePort());
-        System.out.println(req.getRemoteHost());
-        System.out.println(req.getServerPort());
-        System.out.println(req.getServerName());
-        System.out.println(req.getMethod());
+        System.out.println("1 :"+req.getContextPath());
+        System.out.println("2 :"+req.getPathInfo());
+        System.out.println("3 :"+req.getServletPath());
+        System.out.println("4 :"+req.getPathTranslated());
+        System.out.println("5 :"+req.getQueryString());
+        System.out.println("6 :"+req.getRequestURI());
+        System.out.println("7 :"+req.getRequestURL());
+        System.out.println("8 :"+req.getProtocol());
+        System.out.println("9 :"+req.getScheme());
+        System.out.println("10 :"+req.getRemoteAddr());
+        System.out.println("11 :"+req.getRemotePort());
+        System.out.println("12 :"+req.getRemoteHost());
+        System.out.println("13 :"+req.getServerPort());
+        System.out.println("14 :"+req.getServerName());
+        System.out.println("15 :"+req.getMethod());
     }
 }
