3bd8fc3ee9c03fe6f11eee69db2169ac98914301
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 09:59:15 +0530

    updated

--- Mapping_specification/src/main/java/lk/ijse/gdse/hello/api/Default_Mapping.java
+++ Mapping_specification/src/main/java/lk/ijse/gdse/hello/api/Default_Mapping.java
@@ -16,5 +16,24 @@ public class Default_Mapping extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         writer.println("<h1>Hello I am Default Mapping ( / ) </h1>");
+
+        System.out.println("-----------Default Mapping----------");
+        System.out.println("1 : "+req.getContextPath());
+        System.out.println("2 : "+req.getPathInfo());
+        System.out.println("3 : "+req.getServletPath());
+        System.out.println("4 : "+req.getPathTranslated());
+        System.out.println("5 : "+req.getQueryString());
+        System.out.println("6 : "+req.getRequestURI());
+        System.out.println("7 : "+req.getRequestURL());
+        System.out.println("8 : "+req.getProtocol());
+        System.out.println("9 : "+req.getScheme());
+        System.out.println("10 : "+req.getRemoteAddr());
+        System.out.println("11 : "+req.getRemotePort());
+        System.out.println("12 : "+req.getRemoteHost());
+        System.out.println("13 : "+req.getServerPort());
+        System.out.println("14 : "+req.getServerName());
+        System.out.println("15 : "+req.getMethod());
+
+
     }
 }
