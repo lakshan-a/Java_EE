84970c6fb3e109cc194c23b440f33b77797ac019
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-05 10:45:56 +0530

    updated

--- Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
+++ Form_Action/src/main/java/lk/ijse/gdse/hello/api/Form_Action.java
@@ -3,15 +3,21 @@ package lk.ijse.gdse.hello.api;
 import com.sun.xml.internal.ws.transport.http.HttpAdapter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "Exact_Mapping" , value = "/hello")
+
 public class Form_Action extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String id =req.getParameter("id");
+        String name =req.getParameter("name");
+        String address =req.getParameter("address");
+        String salary =req.getParameter("salary");
     }
 }
