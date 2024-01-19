26d0e1a9ac135c6922c6a6a35b6f7a2491156898
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-19 16:28:29 +0530

    updated

--- 02-helloproject/src/main/java/lk/ijse/gdse66/hello/filter/CORSFilter.java
+++ 02-helloproject/src/main/java/lk/ijse/gdse66/hello/filter/CORSFilter.java
@@ -1,13 +1,15 @@
-package lk.ijse.gdse66.hello.fliter;
+package lk.ijse.gdse66.hello.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CORSFliter extends HttpFilter {
+@WebFilter(filterName = "CORSFilter " , urlPatterns = "/*")
+public class CORSFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         String origin = req.getHeader("origin");
--- 04-dbcp/src/main/java/lk/ijse/gdse/hello/dbcp/CustomerServlet.java
+++ 04-dbcp/src/main/java/lk/ijse/gdse/hello/dbcp/CustomerServlet.java
@@ -1,4 +1,4 @@
-package lk.ijse.gdse.hello.api;
+package lk.ijse.gdse.hello.dbcp;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,19 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet(name = "customerServlet",urlPatterns = "/customers")
+@WebServlet(name = "customerServlet" , value = "/customers")
 public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Filter");
-
-
-        System.out.println("CORSFilter");
-        System.out.println("CORSFilter: dispatch to the next filter/servlet");
-        chain.doFilter(req,res);
-
-        System.out.println("CORSFilter : outgoing Response");
+        super.doGet(req, resp);
     }
 }
