44d4ec35aca9192c362b65eee0d3c0501891b1aa
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-19 16:44:47 +0530

    updated

--- 04-dbcp/src/main/java/lk/ijse/gdse/hello/dbcp/AppContextListener.java
+++ 04-dbcp/src/main/java/lk/ijse/gdse/hello/dbcp/AppContextListener.java
@@ -13,6 +13,12 @@ public class AppContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         BasicDataSource dbcp = new BasicDataSource();
+        dbcp.setUsername("root");
+        dbcp.setPassword("12345");
+        dbcp.setUrl("jdbc:mysql://localhost:3306/gdse66_hello");
+        dbcp.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dbcp.setInitialSize(2);
+        dbcp.setMaxTotal(5);
     }
 
     @Override
