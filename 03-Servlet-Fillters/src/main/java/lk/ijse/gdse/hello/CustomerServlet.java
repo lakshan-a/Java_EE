58c694c79f87d8c605040bb31c774cf77b1b56e8
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-19 12:33:29 +0530

    updated

--- 03-Servlet-Fillters/src/main/java/lk/ijse/gdse/hello/CustomerServlet.java
+++ 03-Servlet-Fillters/src/main/java/lk/ijse/gdse/hello/CustomerServlet.java
@@ -13,6 +13,6 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println();
+        System.out.println("Filter");
     }
 }
