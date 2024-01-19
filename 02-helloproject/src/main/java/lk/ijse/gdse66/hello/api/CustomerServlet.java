6a5485661928d1f55fe786924f32d482a3582e05
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-19 10:56:02 +0530

    updated

---
+++ 02-helloproject/src/main/java/lk/ijse/gdse66/hello/api/CustomerServlet.java
@@ -0,0 +1,211 @@
+package lk.ijse.gdse66.hello.api;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.gdse66.hello.dto.CustomerDTO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+
+@WebServlet(name = "Customer", urlPatterns = "/customers", loadOnStartup = 1, initParams = {
+        @WebInitParam(name = "username" , value = "root"),
+        @WebInitParam(name = "password" , value = "12345"),
+        @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/gdse66_hello")
+})
+public class CustomerServlet extends HttpServlet {
+    private String username;
+    private String password;
+    private String url;
+
+    @Override
+    public void init() throws ServletException {
+        /*ServletConfig is used to get configuration information such as database url, mysql username and password*/
+        ServletConfig sc = getServletConfig();
+        username = sc.getInitParameter("username");
+        password = sc.getInitParameter("password");
+        url = sc.getInitParameter("url");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
+
+        /*resp.addHeader("Access-Control-Allow-Origin", "*");*/
+
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+        String id = customerDTO.getId();
+        String name = customerDTO.getName();
+        String address = customerDTO.getAddress();
+
+        /* do some validation */
+        if(id==null || !id.matches("C\\d{3}")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID is empty or invalid");
+            return;
+        } else if (name == null || !name.matches("[A-Za-z ]+")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Name is empty or invalid");
+            return;
+        } else if (address == null || address.length() < 3) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Address is empty or invalid");
+            return;
+        }
+
+        System.out.printf("id=%s, name=%s, address=%s\n", id,name,address);
+
+        /*create a database connection and save data in database*/
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,username,password);
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO customer(id, name, address) VALUES (?,?,?)");
+
+            stm.setString(1,id);
+            stm.setString(2, name);
+            stm.setString(3, address);
+
+            if (stm.executeUpdate() != 0) {
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            }else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to save the customer");
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if(connection !=null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*String action = req.getParameter("action");
+        if(action ==null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "action is empty");
+        } else-if(action.equalsIgnoreCase("GETALL")){
+            getAllCustomers();
+        } else if (action.equalsIgnoreCase("GETONE")) {
+            getCustomerById();
+        }*/
+
+        resp.addHeader("Access-Control-Allow-Origin","*");
+
+        Connection connection = null;
+
+        /*create a database connection and fetch data in database*/
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,username,password);
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM customer");
+            ResultSet rst = stm.executeQuery();
+
+            ArrayList<CustomerDTO> customerList = new ArrayList<>();
+
+            while (rst.next()){
+                String id = rst.getString("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+                System.out.printf("id=%s, name=%s, address=%s\n",id,name,address);
+
+                customerList.add(new CustomerDTO(id, name, address));
+            }
+
+            resp.setContentType("application/json"); //set the MIME type of the content of the response (Thus, add response header called "Content-Type")
+
+            Jsonb jsonb = JsonbBuilder.create();
+            jsonb.toJson(customerList,resp.getWriter());
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if(connection !=null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
+
+        String id = req.getParameter("id");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            PreparedStatement stm = connection.prepareStatement("DELETE FROM customer WHERE id=?");
+            stm.setString(1,id);
+
+            if(stm.executeUpdate() != 0){
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }else{
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to delete the customer!");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }finally{
+            if(connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
+
+        Jsonb jsonb = JsonbBuilder.create();
+        CustomerDTO customerDTO = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+        String id = customerDTO.getId();
+        String name = customerDTO.getName();
+        String address = customerDTO.getAddress();
+
+        System.out.printf("id=%s, name=%s, address=%s\n", id,name,address);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url,username,password);
+            PreparedStatement stm = connection.prepareStatement("UPDATE customer SET name=?, address=? WHERE id=?");
+
+            stm.setString(1, name);
+            stm.setString(2, address);
+            stm.setString(3,id);
+
+            if (stm.executeUpdate() != 0) {
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }else{
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to update the customer");
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if(connection !=null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
