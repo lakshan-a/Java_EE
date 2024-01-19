83a849cbe6d8a544c8eca4aca052b6f00b4c05b4
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-19 12:36:39 +0530

    updated

--- 03-Servlet-Fillters/src/main/java/lk/ijse/gdse/hello/filter/CORSFilter.java
+++ 03-Servlet-Fillters/src/main/java/lk/ijse/gdse/hello/filter/CORSFilter.java
@@ -1,4 +1,19 @@
 package lk.ijse.gdse.hello.filter;
 
-public class CORSFilter {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(filterName = "CORSFilter",urlPatterns = "/customers")
+public class CORSFilter extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("CORSFilter");
+    }
 }
