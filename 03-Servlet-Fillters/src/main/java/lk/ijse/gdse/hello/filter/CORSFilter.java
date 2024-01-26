ad2b06cf133ca39732c1d0b246acae9ecc418ae4
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-01-26 08:40:08 +0530

    updated

--- 03-Servlet-Fillters/src/main/java/lk/ijse/gdse/hello/filter/CORSFilter.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.gdse.hello.filter;
-
-import jakarta.servlet.http.HttpFilter;
-
-public class CORSFilter extends HttpFilter{
-
-
-
-}
---
+++ 03-helloServlet/src/main/java/lk/ijse/gdse66/hello/api/MyServlet1.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse66.hello.api;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        String username = servletContext.getInitParameter("username");
+        System.out.println(username);
+    }
+
+}
