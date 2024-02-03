88ba9870819699453b46ce184b9d484269e2d063
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-03 23:44:55 +0530

    updated

--- Json/src/main/java/lk/ijse/gdse/hello/api/Libraries.java
+++ Json/src/main/java/lk/ijse/gdse/hello/api/Libraries.java
@@ -1,5 +1,9 @@
 package lk.ijse.gdse.hello.api;
 
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObjectBuilder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +16,24 @@ public class Libraries extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
+        objectBuilder.add("id","C001");
+        objectBuilder.add("name","Lakshan");
+
+        JsonObjectBuilder addressObjectBulider =Json.createObjectBuilder();
+        addressObjectBulider.add("no",10);
+        addressObjectBulider.add("street","main street");
+        addressObjectBulider.add("city","Galle");
+
+        objectBuilder.add("address",addressObjectBulider);
+
+        JsonArrayBuilder contactArrayBuilder =Json.createArrayBuilder();
+        contactArrayBuilder.add("0713988628");
+        contactArrayBuilder.add("0716606719");
+
+        objectBuilder.add("contact",contactArrayBuilder);
+
+        resp.getWriter().write(objectBuilder.build().toString());
     }
 }
