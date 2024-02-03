6544aa62e3a1883f11fdff4eb97ffaaeee0046b1
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-03 23:44:34 +0530

    updated

--- Json/src/main/java/lk/ijse/gdse/hello/api/Libraries.java
+++ Json/src/main/java/lk/ijse/gdse/hello/api/Libraries.java
@@ -1,4 +1,17 @@
 package lk.ijse.gdse.hello.api;
 
-public class Libraries {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "jsonLibraries", urlPatterns = "/json")
+public class Libraries extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
